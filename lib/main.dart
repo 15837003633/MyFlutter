2a7f08aa18a14aa5b01a130c9ec8e06ecab89865
scott <>
2023-04-03 22:52:50 +0800

    align containers

--- lib/main.dart
+++ lib/main.dart
@@ -27,67 +27,31 @@ class WDHomeContent extends StatefulWidget {
 class _WDHomeContentState extends State<WDHomeContent> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        MyHomeBody03(),
-        MyHomeBody04(),
-        MyHomeBody05(),
-      ],
-    );
-  }
-}
-
-
-class MyHomeBody00 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(" text text");
-  }
-}
-class MyHomeBody01 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text.rich(
-      TextSpan(
-        children: [
-          TextSpan(text: "《定风波》", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black)),
-          TextSpan(text: "苏轼", style: TextStyle(fontSize: 18, color: Colors.redAccent)),
-          TextSpan(text: "\n莫听穿林打叶声，何妨吟啸且徐行。\n竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。")
-        ],
-      ),
-      style: TextStyle(fontSize: 20, color: Colors.purple),
-      textAlign: TextAlign.center,
-    );
-  }
-}
-
-class MyHomeBody03 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // return Image.asset("resource/image/kkicon.png");
-    return Image(
-      image: AssetImage("resource/image/kkicon.png",),
-      width: 100,
+    return Container(
+      color: Colors.cyan,
+      width: 200,
       height: 200,
-      fit: BoxFit.fill,
+      // alignment: Alignment(0, 0),
+      child: Text("hello",
+        style: TextStyle(color: Colors.red,backgroundColor: Colors.lightBlue),
+      ),
     );
   }
 }
 
-class MyHomeBody04 extends StatelessWidget {
+class AlignDemo extends StatelessWidget {
+  const AlignDemo({
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-        "resource/image/app_icon@2x.png",
-      // width: 300,
+    return Align(
+      child: Text("hello",
+      style: TextStyle(color: Colors.red,backgroundColor: Colors.lightBlue),
+      ),
     );
-
   }
 }
 
-class MyHomeBody05 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Image.network("https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg");
-  }
-}
+
